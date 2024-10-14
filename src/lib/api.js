1f3b32559a0c004607fc20eb46d929adb4a8335c
zThundy__ <antonioantonelli7@gmail.com>
2024-10-14 17:46:40 +0200

    added events listening to html

--- src/lib/api.js
+++ src/lib/api.js
@@ -3,11 +3,22 @@
 class API {
   constructor(res) {
     this.baseUrl = `https://${res}/`;
+    this.events = {};
+
+    window.addEventListener("message", (event) => {
+      console.log(JSON.stringify(event.data, null, 2));
+      if (event.origin !== this.baseUrl) return;
+      if (this.events[event.data.event]) this.events[event.data.event](event.data);
+    });
   }
 
   async get(url) {
     return fetch(`${this.baseUrl}${url}`)
       .then(response => response.json());
+  }
+
+  async registerEvent(event, callback) {
+    this.events[event] = callback;
   }
 
   async post(url, data) {
