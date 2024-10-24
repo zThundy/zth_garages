1fbdc06e50308b46783449aba9d3d486480a5004
zThundy__ <antonioantonelli7@gmail.com>
2024-10-24 19:19:51 +0200

    Create new release

---
+++ .github/ps1/build.ps1
@@ -0,0 +1,7 @@
+$changelogFile = "changelogs.md"
+$changelogContent = Get-Content -Path $changelogFile -Raw
+$versionHeader = "## $version"
+$nextVersionHeader = "## "
+$changelog = $changelogContent -split $versionHeader, 2 | Select-Object -Last 1
+$changelog = $changelog -split $nextVersionHeader, 2 | Select-Object -First 1
+echo "changelog=$changelog" >> $ENV:GITHUB_OUTPUT
